ae00ee6679f8b5b987feed5be8e946810a31028c
Agnes Kim <agneskim4@gmail.com>
2023-06-10 08:45:34 -0400

    update CSS to index and recipes

--- app/__init__.py
+++ app/__init__.py
@@ -10,25 +10,16 @@ migrate = Migrate()
 load_dotenv()
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.static_folder = 'static'
 
-    if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-        # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("RENDER_DATABASE_URI")
-    else:
-        app.config["TESTING"] = True
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
-
-    # Import models here for Alembic setup
     from app.models.recipe import Recipe
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints here
     from .routes import recipes_bp
     app.register_blueprint(recipes_bp, url_prefix="/recipes")
 
